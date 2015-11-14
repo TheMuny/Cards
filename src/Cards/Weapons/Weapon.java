381483d053637ae35c35113c8494b7a9cf3a94a7
Munir <gm.BlackGod@gmail.com>
2015-11-14 20:22:20 +0200

    14.11.2015

--- src/Cards/Weapons/Weapon.java
+++ src/Cards/Weapons/Weapon.java
@@ -2,7 +2,7 @@ package Cards.Weapons;
 
 import Cards.Card;
 
-public abstract class Weapon extends Card {
+public class Weapon extends Card {
 
 	private int durability;
 	private int damage;
--- src/Room/GameRoom.java
+++ src/Room/GameRoom.java
@@ -1,21 +1,36 @@
 package Room;
 
+import java.util.Random;
+
 import Cards.Card;
-import Models.Player;
+import Cards.Creatures.Creature;
+import Cards.Spells.Spell;
+import Cards.Weapons.Weapon;
+import Room.Player;
 
 public class GameRoom implements Models.GameRoom{
 
-	private Player player1;
-	private Player player2;
-	private Card[] tableP1;
-	private Card[] tableP2;
-	private Card[] p1Deck;
-	private Card[] p2Deck;
+	private Player player1 = new Player();
+	private Player player2 = new Player();
+	private Card[] tableP1 = new Card[8];
+	private Card[] tableP2 = new Card[8];
+	private Card[] p1Deck  = new Card[30];
+	private Card[] p2Deck  = new Card[30];
 	
 	
 	
 	public void prepareGame() {
-		
+		for(int i=0;i<p1Deck.length;i++){
+			Random r = new Random();
+			int rand = r.nextInt(100);
+			if(rand>=0&&rand<45){
+				p1Deck[i]=new Creature();
+			}else if(rand>=46&&rand<80){
+				p1Deck[i]=new Spell();
+			}else if(rand>=81&&rand<100){
+				p1Deck[i]=new Weapon();
+			}
+		}
 		
 	}
 	public void changeTurn() {
@@ -25,8 +40,6 @@ public class GameRoom implements Models.GameRoom{
 	
 	
 	public static void main(String[] args) {
-		
-		
 		
 		
 	}
